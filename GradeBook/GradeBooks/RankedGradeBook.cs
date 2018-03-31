460370c8a808c99d37d0a6005f0225d250e8eea9
TK85 <rameentim@hotmail.com>
2018-03-31 10:44:30 -0700

    module 3 updates
    moved the code block into the "else" statement

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -15,24 +15,28 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade (double averageGrade)
         {
             int minimumNumberofStudents = 5;
+
             if (Students.Count < minimumNumberofStudents)
             {
                 throw new InvalidOperationException("Ranked grading requires at least 5 students or more");
             }
-
-            var threshold = (int)Math.Ceiling(Students.Count * 0.2); // 0.2 represents 20%
-            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList(); // LINQ expression, foreach loop can be utilized
-
-            if (grades[threshold - 1]  <= averageGrade)
-                return 'A';
-            else if (grades[(threshold * 2) - 1] <= averageGrade)
-                return 'B';
-            else if (grades[(threshold * 3) - 1] <= averageGrade)
-                return 'C';
-            else if (grades[(threshold * 4) - 1] <= averageGrade)
-                return 'D';
             else
-                return 'F';
+            {
+                var threshold = (int)Math.Ceiling(Students.Count * 0.2); // 0.2 represents 20%
+                var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList(); // LINQ expression, foreach loop can be utilized
+
+                if (grades[threshold - 1] <= averageGrade)
+                    return 'A';
+                else if (grades[(threshold * 2) - 1] <= averageGrade)
+                    return 'B';
+                else if (grades[(threshold * 3) - 1] <= averageGrade)
+                    return 'C';
+                else if (grades[(threshold * 4) - 1] <= averageGrade)
+                    return 'D';
+                else
+                    return 'F';
+            }
+
         }
     }
 }
