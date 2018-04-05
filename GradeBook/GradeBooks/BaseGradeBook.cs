87ade5ded444b05b6394ea6135fde2faf74b9b01
TK85 <rameentim@hotmail.com>
2018-04-04 19:13:36 -0700

    Module 7.5 changes
    solution different from video, but it should work

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -110,21 +110,30 @@ namespace GradeBook.GradeBooks
             }
         }
 
-        public virtual double GetGPA(char letterGrade, StudentType studentType)
+        //start of the changes
+        public virtual int GetGPA(char letterGrade, StudentType studentType)
         {
+            int extrapoint = 0;
+
+            if (IsWeighted)
+            {
+                if (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled)
+                    extrapoint++;
+            }
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + extrapoint;
                 case 'B':
-                    return 3;
+                    return 3 + extrapoint;
                 case 'C':
-                    return 2;
+                    return 2 + extrapoint;
                 case 'D':
-                    return 1;
+                    return 1 + extrapoint;
             }
-            return 0;
+            return 0 + extrapoint;
         }
+        // end of the changes
 
         public virtual void CalculateStatistics()
         {
